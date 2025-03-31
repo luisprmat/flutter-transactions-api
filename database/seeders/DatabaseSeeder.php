804cbafb61d0b0356423d22e7ab3f695e848f8f7
Luis Parrado <luisprmat@gmail.com>
2025-03-31 16:20:31 -0500

    Creating a Category Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // Category::factory()->count(5)->create();
+
+        Category::create(['name' => 'Electronics', 'user_id' => $user->id]);
+        Category::create(['name' => 'Fashion', 'user_id' => $user->id]);
+        Category::create(['name' => 'Home & Garden', 'user_id' => $user->id]);
+        Category::create(['name' => 'Sporting Goods', 'user_id' => $user->id]);
     }
 }
