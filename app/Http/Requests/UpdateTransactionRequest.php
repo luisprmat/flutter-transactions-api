c85c3c7024c24340fddb43c5f5da595e9787e452
Luis Parrado <luisprmat@gmail.com>
2025-03-31 19:10:31 -0500

    Add Transaction CRUD + routes with relationships

---
+++ app/Http/Requests/UpdateTransactionRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateTransactionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'category_id' => ['required', 'integer', Rule::exists('categories', 'id')],
+            'user_id' => ['nullable', 'integer', Rule::exists('users', 'id')],
+            'transaction_date' => ['required', 'date'],
+            'amount' => ['required', 'numeric'],
+            'description' => ['required'],
+        ];
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'name',
@@ -15,5 +20,10 @@ class Category extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
     }
 }
